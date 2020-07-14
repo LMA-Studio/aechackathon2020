0d5ef52ab4e155eb62b492672a3187fea9761d3e
Lisa-Marie Mueller <lmmueller94@gmail.com>
2020-07-13 22:13:00 -0700

    lobby screen and menus, skybox for lobby screen, additional icons

---
+++ StreamVR.Unity/Assets/Scripts/ClickStartUpMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickStartUpMenu : MonoBehaviour
+{
+    public GameObject ConnectionMenu;
+    public GameObject RoomCodeMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowConnectionMenu ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowConnectionMenu ()
+    {
+        RoomCodeMenu.SetActive(false);
+        ConnectionMenu.SetActive(true);
+    }
+
+    public void ShowRoomCodeMenu ()
+    {
+        ConnectionMenu.SetActive(false);
+        RoomCodeMenu.SetActive(true);
+    }
+
+    public void EnterStreamVR()
+    {
+        ConnectionMenu.SetActive(false);
+        RoomCodeMenu.SetActive(false);
+
+        SceneManager.LoadScene("VRGame");
+    }
+}
