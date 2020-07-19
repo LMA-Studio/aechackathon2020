e5875aada8eeb936fa773c35c86605f1e91de305
Lisa-Marie Mueller <lmmueller94@gmail.com>
2020-07-19 10:02:34 -0700

    Revit WPF UI

---
+++ StreamVR.Revit/WPF/StreamVRUI.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LMAStudio.StreamVR.Revit.WPF
+{
+    /// <summary>
+    /// Interaction logic for StreamVRUI.xaml
+    /// </summary>
+    public partial class StreamVRUI : UserControl
+    {
+        public StreamVRUI()
+        {
+            InitializeComponent();
+        }
+    }
+}
